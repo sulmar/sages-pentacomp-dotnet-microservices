e063f86d6d6788f00ebb0844796674e239077bce
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-06-20 12:04:45 +0200

    Pobieranie produktów

--- src/Clients/Blazor.Client/Model/Product.cs
+++ src/Clients/Blazor.Client/Model/Product.cs
@@ -7,16 +7,4 @@ public class Product
     public decimal Price { get; set; }
     public decimal DiscountedPrice  { get; set; }
     public bool IsSale => Price > DiscountedPrice;
-
-    public Product(int id, string name, decimal price, decimal? discountedPrice = null)
-    {
-        Id = id;
-        Name = name;
-        Price = price;
-
-        if (discountedPrice.HasValue)
-            DiscountedPrice = discountedPrice.Value;
-        else
-            DiscountedPrice = Price;
-    }
 }
